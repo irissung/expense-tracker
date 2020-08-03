bdfa4955173fffc5b32f4a410866db851fce32e4
Iris <janyshop@msn.com>
2020-08-03 23:38:15 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
